36224887246d0b2668cacc1e6f612c9987626545
Joey Buczek <joe.buczek@gmail.com>
2015-03-27 01:53:31 +0000

    css class for current user in collab list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
-module ApplicationHelper 
+module ApplicationHelper
+  
+  def bold_if_current_user(user)
+    # Set username to bold text (css class) in the collaborators section for the current user
+    if current_user.name == user.name
+      'highlight-name'
+    end
+  end
+  
 end
